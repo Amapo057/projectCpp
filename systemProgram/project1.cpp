035f977c7b5de9b6874ccc6124291b5f118272f9
Amapo057 <jiseong2002@naver.com>
2025-09-29 21:28:17 +0900

    start systemproject

--- systemProgram/project1.cpp
+++ systemProgram/project1.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <bitset>
 using namespace std;
 
 
 
-int main(){
+string hexToBin(const string&);
 
+int main(){
+    string inputHex = "AAAAAA";
+
+    string bin;
+    bin = hexToBin(inputHex);
+    string op;
+    op = bin.substr(0, 6);
+    string flagBit;
+    flagBit = bin.substr(6, 2);
+
+    cout << bin << endl;
+    cout << op << endl;
+    cout << flagBit << endl;
+    // cout << adMode << endl;
+    // cout << disp << endl;
+    // cout << ta << endl;
+    // cout << regA << endl;
+}
+
+
+string hexToBin(const string& hexValue){
+    unsigned long intValue;
+    stringstream ss;
+    ss << hex << hexValue;
+    ss >> intValue;
+
+    bitset<24> bits(intValue);
+
+    return bits.to_string();
 }
