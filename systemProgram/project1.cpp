c00d7a74de879626538acf073e3bf86188ac273d
김지성 <jiseong2002@naver.com>
2025-09-29 23:27:01 +0900

    split flag

--- systemProgram/project1.cpp
+++ systemProgram/project1.cpp
@@ -9,20 +9,31 @@ using namespace std;
 string hexToBin(const string&);
 
 int main(){
-    string inputHex = "AAAAAA";
+    string inputHex = "cf3c0003";
 
     string bin;
     bin = hexToBin(inputHex);
     string op;
     op = bin.substr(0, 6);
-    string flagBit;
-    flagBit = bin.substr(6, 2);
+    string ni;
+    ni = bin.substr(6, 2);
+    string x;
+    x = bin.substr(8, 1);
+    string bp;
+    bp = bin.substr(9, 2);
+    string e;
+    e = bin.substr(11, 1);
+
+
+    string disp;
+    disp = bin.substr(12, 20);
+
 
     cout << bin << endl;
     cout << op << endl;
-    cout << flagBit << endl;
+    cout << "flagbit :" << ni << x << bp << e << endl;
     // cout << adMode << endl;
-    // cout << disp << endl;
+    cout << disp << endl;
     // cout << ta << endl;
     // cout << regA << endl;
 }
@@ -34,7 +45,7 @@ string hexToBin(const string& hexValue){
     ss << hex << hexValue;
     ss >> intValue;
 
-    bitset<24> bits(intValue);
+    bitset<32> bits(intValue);
 
     return bits.to_string();
 }
