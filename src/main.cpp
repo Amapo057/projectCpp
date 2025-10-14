c8fb4d9752cd62de9c053ec10dc27691b0b20fe5
Amapo057 <jiseong2002@naver.com>
2025-10-14 16:58:36 +0900

    필수 멤버 함수 테스트

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 //g++ ./src/main.cpp -o ./bin/main ; ./bin/main
 
+// 함수 선언
 class Circle {
     private:
         double radius;
@@ -14,13 +15,11 @@ class Circle {
 		Circle(double radius);
         Circle();
         Circle(const Circle& circle);
+        
         ~Circle ();
 };
 
-// double Circle :: getRadius () const{
-//     return radius;
-// }
-
+// 생성자
 Circle :: Circle (double rds)
 :radius(rds)
 {
@@ -32,7 +31,6 @@ Circle :: Circle ()
 {
     cout << radius << endl;
     cout << "none" << endl;
-
 }
 Circle :: Circle (const Circle& cr)
 :radius(cr.radius)
@@ -40,10 +38,13 @@ Circle :: Circle (const Circle& cr)
     cout << radius << endl;
     cout << "point" << endl;
 }
+
+// 소멸자
 Circle :: ~Circle(){
     cout << "call destructor" << endl;
 }
 
+// 함수 정의
 double Circle :: getArea () const{
     const double PI = 3.14;
     return (PI * radius * radius);
@@ -65,14 +66,14 @@ int main(){
     cout << endl;
 
     Circle circle2(circle1);
-    cout << "R " << circle1.getRadius() << endl;
-    cout << "A " << circle1.getArea() << endl;
-    cout << "P " << circle1.getPerimeter() << endl << endl;
+    cout << "R " << circle2.getRadius() << endl;
+    cout << "A " << circle2.getArea() << endl;
+    cout << "P " << circle2.getPerimeter() << endl << endl;
 
     Circle circle3;
-    cout << "R " << circle1.getRadius() << endl;
-    cout << "A " << circle1.getArea() << endl;
-    cout << "P " << circle1.getPerimeter() << endl;
+    cout << "R " << circle3.getRadius() << endl;
+    cout << "A " << circle3.getArea() << endl;
+    cout << "P " << circle3.getPerimeter() << endl;
     cout << endl;
 
     return 0;
